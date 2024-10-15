1e3d167d6a732d6b6d050832f9198200c72ab6df
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2024-10-15 23:46:48 +0500

    First Commit

---
+++ temp.js
@@ -0,0 +1,95 @@
+// Variables;
+
+// import './App.css';
+
+
+// function Person(props){
+//   return(
+//     <>
+//       <p>My Name is {props.name}</p>
+//       <p>I am from {props.country}</p>
+//       <p>I am {props.age} Years Old</p>
+//     </>
+//   )
+// }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Person name={'Ahmed'} country={'Yemen'} age={'22'} />
+//       <Person name={'Salim'} country={'Qassim'} age={'24'} />
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+// 2: HOOOOOOOOOOOOOOOOOOOKSSS
+
+// import { useState } from 'react';
+// import './App.css';
+
+
+
+// function App() {
+
+//   const [counter, setCounter] = useState(0);
+  
+//   return (
+//     <div className="App">
+//       <button onClick={() => setCounter((prevCounter)=> prevCounter-1)}>-</button>
+//       <h1>{counter}</h1>
+//       <button onClick={() => setCounter((prevCounter)=> prevCounter+1)}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The third one.
+// import { useState, useEffect } from 'react';
+// import './App.css';
+
+
+
+// function App() {
+
+//   const [counter, setCounter] = useState(0);
+
+//   // useEffect(() =>{
+//   //   alert("Hello Ahmed, you've changed the code to: "+ counter)
+//   // }, [counter])
+  
+
+  
+//   return (
+//     <div className="App">
+//       <button onClick={() => setCounter((prevCounter)=> prevCounter-1)}>-</button>
+//       <h1>{counter}</h1>
+//       <button onClick={() => setCounter((prevCounter)=> prevCounter+1)}>+</button>
+//     </div>
+//   );
+// }
+
+// export default App;
